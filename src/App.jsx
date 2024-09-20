72c09885d92c729c518c344bddfd5be6fe29edb7
rishwankk <rishwan063@gmail.com>
2024-09-20 19:22:26 +0530

    initial configuration completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './layout.scss'; 
+
+const App = () => {
+  return (
+    <div className="layout">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
